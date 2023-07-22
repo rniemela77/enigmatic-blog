3d1bee405125bf13061b4187017e3e99d623a605
Robert Niemela <84466763+rniemela77@users.noreply.github.com>
2023-07-22 04:34:06 +0000

    wire up first route between web.php and .vue

---
+++ blog/resources/js/Pages/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+      Welcome to Laravel with Inertia.js!
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      foo: String,
+    },
+  }
+  </script>
+  
